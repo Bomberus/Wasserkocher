3480940df6eecaace6d37110095410230132b6a7
Pascal Maximilian Bremer <maximilian@cdsffo.de>
2016-05-25 18:22:14 +0200

    First Prototype

--- Wasserkocher.asm
+++ Wasserkocher.asm
@@ -14,20 +14,75 @@ ajmp init
 ; Hauptprogramm
 ;-----------------
 init:
-	mov r0,#14h ;Temperatur = 20
-	mov r1,#4bh ;Wassermenge = 75 dl
+	;Aliases
+	temp_current equ R0
+	temp_max equ P2
+	taster equ P0.0
+	hupe equ P0.1
+	heizstab equ P0.2
+	water_current equ R1
+	water_min equ R2
+	is_active equ P0.3
+	hupe_count equ R3
+	;------------------------------------
+	;Simulation
+	mov temp_current,#20 ;Temperatur = 20
+	mov water_current,#75 ;Wassermenge = 75 dl
+	mov water_min, #50 ;Wassermenge
+	clr heizstab ;Disable Heizstab
+	clr is_active
+	;-----------------------------------
+	;Test Values
+	mov temp_max, #22
+
+main:
+	jnb taster, disable
+	call auslesen;
+
+	;check temperature
+	mov A, temp_current
+	clr cy ;Reset the Carry
+	subb A, temp_max ;(aktuelle - gewuenschte Temp) cy=1, wenn aktuell < gewünscht
+	clr A
+	jnb cy, disable
+
+	;check water
+	mov A, water_min
+	clr cy ;Don't forget to reset that motherfucker
+	subb A, water_current
+	clr A
+	jnb cy,disable
+	clr cy
+
+	setb is_active
+	call erhitzen
+	jmp main
+
 ; Lies derzeitige Füllmenge und Temperatur aus
 ;-----------------
 auslesen:
+	RET
 
-main:
-	jnb P0.0, main
-	mov A, R0
-	cjne A, P2, main
-display:
+disable:
+	jnb is_active, main; Wenn der Wasserkocher von aktiv in deaktiv wechselt Hupe!
+	clr heizstab ; deaktiviere Heizstab
+	clr is_active
+	mov hupe_count,#10
+	call signal
+	jmp main
 
 erhitzen:
-
+	setb heizstab
+	mov A,temp_current
+	add A,#2 ;Add 2°C to the water temperature
+	mov temp_current,A
+	clr A
+	ret
 signal:
+	setb hupe ;aktiviere Hupe
+	djnz hupe_count, signal
+	clr hupe
+	ret
+display:
 
 end
