ecee5a63b9488e0b8164ece1538b4530731443d6
Fabian Retkowski <fabian.retkowski@sap.com>
2016-06-06 23:56:19 +0200

    Temperaturanzeige

--- Wasserkocher.asm
+++ Wasserkocher.asm
@@ -33,7 +33,7 @@ init:
 	clr is_active
 	;-----------------------------------
 	;Test Values
-	mov temp_max, #22
+	mov temp_max, #30
 
 main:
 	jnb taster, disable
@@ -42,7 +42,7 @@ main:
 	;check temperature
 	mov A, temp_current
 	clr cy ;Reset the Carry
-	subb A, temp_max ;(aktuelle - gewuenschte Temp) cy=1, wenn aktuell < gewünscht
+	subb A, temp_max ;(aktuelle - gewuenschte Temp) cy=1, wenn aktuell < gewÃ¼nscht
 	clr A
 	jnb cy, disable
 
@@ -56,9 +56,10 @@ main:
 
 	setb is_active
 	call erhitzen
+	call zeigen
 	jmp main
 
-; Lies derzeitige Füllmenge und Temperatur aus
+; Lies derzeitige FÃ¼llmenge und Temperatur aus
 ;-----------------
 auslesen:
 	RET
@@ -74,7 +75,7 @@ disable:
 erhitzen:
 	setb heizstab
 	mov A,temp_current
-	add A,#2 ;Add 2°C to the water temperature
+	add A,#2 ;Add 2Â°C to the water temperature
 	mov temp_current,A
 	clr A
 	ret
@@ -83,6 +84,40 @@ signal:
 	djnz hupe_count, signal
 	clr hupe
 	ret
+zeigen:
+	mov DPTR,#TABLE
+	mov a,temp_current ; aktuelle Temperatur
+	mov b, #0ah
+	div ab
+	mov r6, a; save a
+	movc a,@a+dptr
+	mov r4, a ; Stelle 1
+	mov a, r6; restore a
+	xch a,b
+	movc a,@a+dptr
+	mov r5, a ; Stelle 2
+	call display
+	ret;
+;;;;;;Display - Ausgabe auf den Port P1 - aktivieren mit Port P3.0 und P3.1
 display:
+	; letzte Ziffer
+	mov P1, r5
+	clr P3.0
+	setb P3.0
+	; vorletzte Ziffer
+	mov P1, r4
+	clr p3.1
+	setb P3.1
+	ret ; return
+;;;;;
+
+org 300h
+table:
+DB 11000000b
+DB 11111001b, 10100100b, 10110000b
+DB 10011001b, 10010010b, 10000010b
+DB 11111000b, 10000000b, 10010000b
+end
+
 
 end
